489d9e21569c0ae3254d7406aefe563d894d0f63
Thiago da Silva Alves <avsthiago@gmail.com>
2021-11-10 14:11:40 +0100

    Adding sonar fail file

--- src/main_flake_fail.py
+++
@@ -1,5 +0,0 @@
-def hello_world():
-    print("hello world")  # dsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasda
-    file = open('example.py', 'r')
-    a = True
-    return False if a == True else True
