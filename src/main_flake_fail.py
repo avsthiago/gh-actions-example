21919e60a6bc06a8235e88ca000ce1d26fde2657
Thiago da Silva Alves <avsthiago@gmail.com>
2021-11-10 13:59:43 +0100

    Adding some bugs for sonarqube

--- src/main_flake_fail.py
+++ src/main_flake_fail.py
@@ -1,3 +1,5 @@
 def hello_world():
     print("hello world")  # dsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasdadsadasdadsadasda dsadasda dsadasda dsadasda dsadasda
-    
+    file = open('example.py', 'r')
+    a = True
+    return False if a == True else True
